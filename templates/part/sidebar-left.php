f212e6f83577fa84b325a8ec767aa4f4e1fbf4d9
Crystal Barton <cbarto11@uncc.edu>
2014-08-02 02:16:30 -0400

    fix sidebar sliding action for mobile versions.

--- templates/part/sidebar-left.php
+++ templates/part/sidebar-left.php
@@ -6,8 +6,8 @@ $widgets = wp_get_sidebars_widgets();
 if( array_key_exists('uncc-left-sidebar', $widgets) && count($widgets['uncc-left-sidebar']) ):
 ?>
 
-<div id="left-sidebar-wrapper" class="left sidebar wrapper clearfix">
-	<div id="left-sidebar" class="left sidebar clearfix">
+<div id="left-sidebar-wrapper" class="sidebar-wrapper clearfix">
+	<div id="left-sidebar" class="sidebar clearfix">
 	
 		<?php if( $uncc_mobile_support->use_mobile_site ): ?><h2>Left Sidebar</h2><?php endif; ?>
 		<?php dynamic_sidebar( 'uncc-left-sidebar' ); ?>
--- templates/part/sidebar-right.php
+++ templates/part/sidebar-right.php
@@ -6,8 +6,8 @@ $widgets = wp_get_sidebars_widgets();
 if( array_key_exists('uncc-right-sidebar', $widgets) && count($widgets['uncc-right-sidebar']) ):
 ?>
 
-<div id="right-sidebar-wrapper" class="right sidebar wrapper clearfix">
-	<div id="right-sidebar" class="right sidebar clearfix">
+<div id="right-sidebar-wrapper" class="sidebar-wrapper clearfix">
+	<div id="right-sidebar" class="sidebar clearfix">
 	
 		<?php if( $uncc_mobile_support->use_mobile_site ): ?><h2>Right Sidebar</h2><?php endif; ?>
 		<?php dynamic_sidebar( 'uncc-right-sidebar' ); ?>
