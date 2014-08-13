821ac46b69c852fc7ae7c51dda9beba6f53a0573
Crystal Barton <cbarto11@uncc.edu>
2014-08-13 06:37:38 -0400

    add pagination.

---
+++ templates/other/pagination.php
@@ -0,0 +1,20 @@
+
+<?php global $nh_config, $nh_mobile_support, $nh_template_vars, $wp_query; ?>
+
+<?php
+if( $wp_query->max_num_pages > 1 ):
+
+	?>
+	<div id="page-navigation" class="clearfix" role="navigation">
+		<div class="nav-next">
+			<?php next_posts_link( '&laquo; Older Posts' ); ?>
+		</div>
+		<div class="nav-prev">
+			<?php previous_posts_link( 'Newer Posts &raquo;' ); ?>
+		</div>
+	</div>
+	<?php
+
+endif; // if( $wp_query->max_num_pages > 1 )
+
+
