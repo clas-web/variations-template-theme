6f769c6df2242088142059950574a498995c44e7
Crystal Barton <cbarto11@uncc.edu>
2014-08-17 07:39:32 -0400

    add default variation mobile links.

--- templates/part/footer.php
+++ templates/part/footer.php
@@ -3,4 +3,24 @@
 <?php global $uncc_config, $uncc_mobile_support, $uncc_template_vars; ?>
 
 
-<?php
+<div id="footer-wrapper" class="clearfix">
+	<div id="footer" class="clearfix">
+
+
+<?php if( $uncc_mobile_support->is_mobile || $uncc_mobile_support->use_mobile_site ): ?>
+
+	<div class="mobile-links">
+
+	<?php if( $uncc_mobile_support->use_mobile_site ): ?>
+		<a href="<?php echo uncc_get_page_url(); ?>?full">Full Site</a> | Mobile Site
+	<?php else: ?>
+		Full Site | <a href="<?php echo uncc_get_page_url(); ?>?mobile">Mobile Site</a>
+	<?php endif; ?>
+	
+	</div> <!-- .mobile-links -->
+
+<?php endif; ?>
+		
+	</div><!-- #footer -->
+</div><!-- #footer-wrapper -->
+
