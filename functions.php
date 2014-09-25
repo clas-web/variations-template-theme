1937a2f42c4298d0e4c1e43cf017fac38d12e3ba
Crystal Barton <cbarto11@uncc.edu>
2014-09-25 04:52:30 -0400

    refactor taxonomy list code.

--- functions.php
+++ functions.php
@@ -1096,32 +1096,18 @@ function uncc_get_taxonomy_list( $taxonomy_name, $post )
 	$html = '';
 	$html .= '<div class="taxonomy-list '.$taxonomy->name.'-list">';	
 
-	if ($taxonomy->label == "Categories") 
+	$taxonomy_label = $taxonomy->label;
+
+	if( $taxonomy->label == "Categories" ) 
 	{
 		$taxonomy_label = get_option('category_base');
-
-		if( isset( $taxonomy_label )  && $taxonomy_label !="" )
-		{
-			$taxonomy_label = get_option('category_base');
-		}
-		else
-		{
-			$taxonomy_label = $taxonomy->label;
-		}
+		if( !$taxonomy_label ) $taxonomy_label = $taxonomy->label;
 		$taxonomy_label_style = "<span style='category-label'>";
 	} 
-	else if ($taxonomy->label == "Tags") 
+	else if( $taxonomy->label == "Tags" ) 
 	{
 		$taxonomy_label = get_option('tag_base');
-
-		if( isset( $taxonomy_label )  && $taxonomy_label !="" )
-		{
-			$taxonomy_label = get_option('tag_base');
-		}
-		else
-		{
-			$taxonomy_label = $taxonomy->label;
-		}
+		if( !$taxonomy_label ) $taxonomy_label = $taxonomy->label;
 		$taxonomy_label_style = "<span style='tag-label'>";
 	}
 	else
