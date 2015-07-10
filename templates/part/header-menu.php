9071491f420d9216ca09dcddaa6c034d0fc12139
Crystal Barton <cbarto11@uncc.edu>
2015-07-10 07:34:33 -0400

    remove obsolete mobile code for old mobile menus.

--- templates/part/header-menu.php
+++ templates/part/header-menu.php
@@ -14,19 +14,7 @@ $header_menu = wp_nav_menu(
 <div id="header-menu-wrapper" class="clearfix">
 	<div id="header-menu">
 
-	<?php if( $vtt_mobile_support->use_mobile_site ): ?>
-		<h2 class="search">Search</h2>
-		<form id="site-searchform" role="search" method="get" class="searchform" action="<?php echo home_url( '/' ); ?>">
-			<script>var main_search_used = false;</script>
-			<div class="textbox_wrapper">
-				<input type="text" name="s" id="header-search" class="s" size="30" value="<?php if( is_search() ) { the_search_query(); } else { echo "Search this site"; } ?>" onfocus="if (!main_search_used) { this.value = ''; main_search_used = true; }" />
-				<input type="submit" id="searchsubmit" value="Search">
-			</div>
-		</form><!-- #site-searchform -->
-	<?php endif; ?>
-
 	<?php if( strpos( $header_menu, '</li>' ) !== false ): ?>
-		<?php if( $vtt_mobile_support->use_mobile_site ): ?><h2 class="menu">Menu</h2><?php endif; ?>
 		<?php echo $header_menu; ?>
 	<?php endif; ?>
 
