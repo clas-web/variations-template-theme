cc87983598d5c1eb9b37a0e2fe84fdced089f3b6
Crystal Barton <atrus1701@gmail.com>
2015-10-01 12:19:08 -0400

    fix Full-Text Search title case error.

--- search.php
+++ search.php
@@ -8,5 +8,5 @@
 // vtt_print( 'PAGE:search.php' );
 vtt_set_page_content_type( 'search' );
 vtt_set_page_title( 'Search for: '.get_search_query() );
-vtt_set_page_listing_name( 'FUll-Text Search' );
+vtt_set_page_listing_name( 'Full-Text Search' );
 vtt_render_page();
