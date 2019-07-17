51ef556660a26bcf3248ddb1ef4094462dc370a3
rmccal14@uncc.edu <rmccal14@uncc.edu>
2019-07-17 14:27:21 -0400

    Remove wide_header check

--- variations/light/templates/part/main.php
+++ variations/light/templates/part/main.php
@@ -1,5 +1,6 @@
-<?php // vtt_print('default:part:main');
+<?php // vtt_print('default:part:main'); ?>
 
+<?php
 // From News Hub variation, determine if Featured Story option is selected
 global $nhs_section;
 $featured = 'not featured';
@@ -13,33 +14,38 @@ if ( is_single() ) {
 			$featured = $featured[0];
 		}
 	}
-
-	$nhs_section = nhs_get_wpquery_section();
-	$post        = $nhs_section->get_single_story( $post );
-	extract( $post->nhs_data );
-	$wide_header = false;
-
-	switch ( $nhs_section->thumbnail_image ) :
-		case 'landscape':
-		case 'normal':
-		case 'embed':
-			if ( $image ) :
-				$wide_header = true;
-			endif;
-			break;
-		default:
-			break;
-	endswitch;
 }
 
-/* From News Hub variation */
+	// if ( ! function_exists( 'nhs_get_wpquery_section' ) ) {
+		// print_r( $post );
+// } else {
+	// $nhs_section = nhs_get_wpquery_section();
+	// 	$post        = $nhs_section->get_single_story( $post );W
+	// 	extract( $post->nhs_data );
+	// 	$wide_header = false;
+
+	// 	switch ( $nhs_section->thumbnail_image ) :
+	// 		case 'landscape':
+	// 		case 'normal':
+	// 		case 'embed':
+	// 			if ( $image ) :
+	// 				$wide_header = true;
+	// 			endif;
+	// 			break;
+	// 		default:
+	// 			break;
+	// endswitch;
+	// }
+// }
+
+
 ?>
 
 <div id="main-wrapper" class="clearfix">
 
 	<?php
-		// Using Featured Story from News Hub variation
-	if ( $wide_header && 'featured' === $featured ) :
+	// Using Featured Story from News Hub variation
+	if ( 'featured' === $featured ) :
 		?>
 		<div class="feature-wrapper break-out">
 			<div class="wide-header" title="Featured Image" style="background-image:url(<?php echo $image; ?>)"></div>
@@ -49,54 +55,42 @@ if ( is_single() ) {
 				</div>
 			</div>
 		</div>
-		<div id="main" class="feature">
-			<div id="full-menu" class="hide">
-			<?php vtt_get_template_part( 'sidebar', 'part', vtt_get_queried_object_type() ); ?>
+		<div id="main" class="feature" role="main">
+			<!-- <div id="full-menu" class="hide">			 -->
 
-			<?php
-			// vtt_get_template_part( 'sidebar', 'part', vtt_get_queried_object_type() );
-			// vtt_get_theme_file_path( 'variations/light/templates/part/page.php' );
-			/* From News Hub variation */
-			?>
-
-
-
-			<?php
-	else :
+		<?php
 		// Not using Featured Story
-		?>
-		
-
-			<div id="main" role="main">
-				<?php
-					echo '<div id="full-menu" class="hide">';
-
-					// Add header menu if needed
-				if ( ! has_nav_menu( 'header-navigation' ) ) {
-					vtt_get_template_part( 'header-menu', 'part', vtt_get_queried_object_type() );
-
-				}
-
-					// Add sidebars if needed
-				if ( vtt_get_theme_file_path( 'templates/part/sidebar.php' ) ) {
-					vtt_get_template_part( 'sidebar', 'part', vtt_get_queried_object_type() );
-				} else {
-					if ( vtt_get_theme_file_path( 'templates/part/sidebar-left.php' ) ) {
-						vtt_get_template_part( 'sidebar-left', 'part', vtt_get_queried_object_type() );
-					}
-					if ( vtt_get_theme_file_path( 'templates/part/sidebar-right.php' ) ) {
-						vtt_get_template_part( 'sidebar-right', 'part', vtt_get_queried_object_type() );
-					}
-				}
-
-					endif;
-	?>
-
-			</div><!-- #full-menu -->
-
-			<?php
-				// Get content.php
-				vtt_get_template_part( 'content', 'part', vtt_get_queried_object_type() );
+		else :
 			?>
-		</div><!-- #main -->
-	</div><!-- #main-wrapper -->
+			<div id="main" role="main">
+			<?php
+		endif;
+
+			echo '<div id="full-menu" class="hide">';
+
+			// Add header menu if needed
+		if ( ! has_nav_menu( 'header-navigation' ) ) {
+			vtt_get_template_part( 'header-menu', 'part', vtt_get_queried_object_type() );
+		}
+
+			// Add sidebars if needed
+		if ( vtt_get_theme_file_path( 'templates/part/sidebar.php' ) ) {
+			vtt_get_template_part( 'sidebar', 'part', vtt_get_queried_object_type() );
+		} else {
+			if ( vtt_get_theme_file_path( 'templates/part/sidebar-left.php' ) ) {
+				vtt_get_template_part( 'sidebar-left', 'part', vtt_get_queried_object_type() );
+			}
+			if ( vtt_get_theme_file_path( 'templates/part/sidebar-right.php' ) ) {
+				vtt_get_template_part( 'sidebar-right', 'part', vtt_get_queried_object_type() );
+			}
+		}
+
+		?>
+		</div><!-- #full-menu -->
+
+		<?php
+		// Get content.php
+		vtt_get_template_part( 'content', 'part', vtt_get_queried_object_type() );
+		?>
+	</div><!-- #main -->
+</div><!-- #main-wrapper -->
